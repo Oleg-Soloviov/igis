f69f52075b075df0a7f4003a988f6fad42a874e5
Oleg Soloviov <osoloviov@list.ru>
2018-01-29 17:31:51 +0400

    Добавил поля поиска в админку больниц.

--- igis_udm/admin.py
+++ igis_udm/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from .models import Place, Hospital, Person, Speciality, SEOitems
 
+
+class HospitalAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'name']
+
+
 admin.site.register(Place)
-admin.site.register(Hospital)
+admin.site.register(Hospital, HospitalAdmin)
 admin.site.register(Person)
 admin.site.register(Speciality)
 admin.site.register(SEOitems)
-
